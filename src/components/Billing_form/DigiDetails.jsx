7799701a66e27bae45cf51410063963a2ad53f2c
sidharth-agarwal <sidharthagarwal.2003@gmail.com>
2024-11-28 01:28:36 +0530

    Updated digi details

--- src/components/Billing_form/DigiDetails.jsx
+++ src/components/Billing_form/DigiDetails.jsx
@@ -1,24 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../firebaseConfig";
+import React, { useState } from "react";
 
 const DigiDetails = ({ onNext, onPrevious }) => {
   const [data, setData] = useState({
     isDigiUsed: false,
     digiDie: "",
   });
-
-  const [digiDieOptions, setDigiDieOptions] = useState([]);
-
-  useEffect(() => {
-    const fetchDigiDies = async () => {
-      const querySnapshot = await getDocs(collection(db, "digiDies"));
-      const digiDies = querySnapshot.docs.map((doc) => doc.data());
-      setDigiDieOptions(digiDies);
-    };
-
-    fetchDigiDies();
-  }, []);
 
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
@@ -37,6 +23,7 @@ const DigiDetails = ({ onNext, onPrevious }) => {
   return (
     <form onSubmit={handleSubmit} className="space-y-4">
       <h2 className="text-xl font-bold text-gray-700 mb-4">Digi Details</h2>
+      {/* Checkbox for Digi Usage */}
       <label className="flex items-center">
         <input
           type="checkbox"
@@ -47,9 +34,10 @@ const DigiDetails = ({ onNext, onPrevious }) => {
         />
         Is Digi being used?
       </label>
+      {/* Conditional Dropdown for Digi Die */}
       {data.isDigiUsed && (
         <div>
-          <label>Select Digi Die:</label>
+          <label className="block mb-2 font-medium">Select Digi Die:</label>
           <select
             name="digiDie"
             value={data.digiDie}
@@ -57,14 +45,12 @@ const DigiDetails = ({ onNext, onPrevious }) => {
             className="border rounded-md p-2 w-full"
           >
             <option value="">Select Digi Die</option>
-            {digiDieOptions.map((die, index) => (
-              <option key={index} value={die.name}>
-                {die.name}
-              </option>
-            ))}
+            <option value="12x18">12x18</option>
+            <option value="13x19">13x19</option>
           </select>
         </div>
       )}
+      {/* Navigation Buttons */}
       <div className="flex justify-between">
         <button
           type="button"
