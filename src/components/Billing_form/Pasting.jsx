5115d48832eb1bdc061425de48f0350d65fcef4c
sidharth-agarwal <sidharthagarwal.2003@gmail.com>
2024-12-04 22:34:15 +0530

    unnecessary code removed

--- src/components/Billing_form/Pasting.jsx
+++ src/components/Billing_form/Pasting.jsx
@@ -1,36 +1,3 @@
-// import React from "react";
-
-// const Pasting = ({ onPrevious, onNext }) => {
-//   const handleNext = (e) => {
-//     e.preventDefault(); // Prevent default form submission
-//     onNext(); // Move to the ReviewAndSubmit component
-//   };
-
-//   return (
-//     <form onSubmit={handleNext} className="space-y-4">
-//       <h2 className="text-xl font-bold text-gray-700 mb-4">Pasting</h2>
-//       <p>No fields to populate in this section.</p>
-//       <div className="flex justify-between mt-6">
-//         <button
-//           type="button"
-//           onClick={onPrevious}
-//           className="bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600"
-//         >
-//           Previous
-//         </button>
-//         <button
-//           type="submit"
-//           className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
-//         >
-//           Next
-//         </button>
-//       </div>
-//     </form>
-//   );
-// };
-
-// export default Pasting;
-
 import React from "react";
 
 const Pasting = ({ state, dispatch, onPrevious, onNext }) => {
