605d25c9b43d46abd8a614f3a21fa8cf59ed2312
sidharth-agarwal <sidharthagarwal.2003@gmail.com>
2024-12-06 00:52:15 +0530

    removed useless car

--- src/components/Estimates/EstimateDetails.jsx
+++ src/components/Estimates/EstimateDetails.jsx
@@ -1,33 +1,3 @@
-// import React, { useState } from "react";
-// import EstimateDetails from "./EstimateDetails";
-
-// const GroupDropdown = ({ clientName, projectName, estimates }) => {
-//   const [openDropdown, setOpenDropdown] = useState(false);
-
-//   return (
-//     <div className="border rounded-lg p-4 shadow-md">
-//       <div
-//         onClick={() => setOpenDropdown(!openDropdown)}
-//         className="cursor-pointer bg-gray-100 px-4 py-2 rounded-md flex justify-between items-center"
-//       >
-//         <h3 className="font-semibold text-lg">
-//           {clientName} - {projectName}
-//         </h3>
-//         <span>{openDropdown ? "▼" : "▶"}</span>
-//       </div>
-//       {openDropdown && (
-//         <div className="mt-4 space-y-4">
-//           {estimates.map((estimate) => (
-//             <EstimateDetails key={estimate.id} estimate={estimate} />
-//           ))}
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default GroupDropdown;
-
 import React from "react";
 import SectionWrapper from "./SectionWrapper";
 
