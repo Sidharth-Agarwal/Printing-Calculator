23d12c29e946321f043b4f2b562ba2cf62e678a3
sidharth-agarwal <sidharthagarwal.2003@gmail.com>
2025-05-22 02:20:59 +0530

    updated lead details modal and removed action buttons

--- src/components/CRM/LeadRegistration/LeadDetailsModal.jsx
+++ src/components/CRM/LeadRegistration/LeadDetailsModal.jsx
@@ -212,67 +212,6 @@ const LeadDetailsModal = ({
                 </div>
               </div>
             )}
-            
-            {/* Actions */}
-            <div className="mt-6 border-t border-gray-200 pt-4 flex flex-wrap gap-2 justify-between">
-              <div className="flex gap-2">
-                <CRMActionButton
-                  type="secondary"
-                  onClick={() => onEdit(lead)}
-                  icon={
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                    </svg>
-                  }
-                >
-                  Edit Lead
-                </CRMActionButton>
-                
-                <CRMActionButton
-                  type="primary"
-                  onClick={() => onAddDiscussion(lead)}
-                  icon={
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
-                    </svg>
-                  }
-                >
-                  Add Discussion
-                </CRMActionButton>
-              </div>
-              
-              <div className="flex gap-2">
-                {!isConverted && (
-                  <CRMActionButton
-                    type="info"
-                    onClick={handleAdvanceStatus}
-                    isLoading={isUpdatingStatus}
-                    disabled={isUpdatingStatus}
-                    icon={
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 7l5 5m0 0l-5 5m5-5H6" />
-                      </svg>
-                    }
-                  >
-                    Advance Status
-                  </CRMActionButton>
-                )}
-                
-                {canConvert && (
-                  <CRMActionButton
-                    type="success"
-                    onClick={() => onConvert(lead)}
-                    icon={
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
-                      </svg>
-                    }
-                  >
-                    Convert to Client
-                  </CRMActionButton>
-                )}
-              </div>
-            </div>
           </div>
           
           {/* Discussion History */}
